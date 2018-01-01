cd54326285a28c12d04a9bed777e66eddf1ba7e5
Jevgenijs Andrejevs <evgean@gmail.com>
2018-01-01 15:37:41 +0200

    JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j;
 
+/**
+*Class который пока не понятно для чего
+*@author Jevgenijs
+*@since 01.01.2018
+*@version 1
+*/
+
 public class Calculate{
+	
+	/**
+	* метод позволяющий вывести "hello world"
+	*@return nothing
+	*/
+	
 	public static void main (String[] args){
 		System.out.println("Hello world of tanks.");
 	}
