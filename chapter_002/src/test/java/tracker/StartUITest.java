f4a0e97cb467818f30ae70fff57da0c22da51545
Jevgenijs Andrejevs <evgean@gmail.com>
2018-02-19 12:16:08 +0200

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/tracker/StartUITest.java
+++ chapter_002/src/test/java/tracker/StartUITest.java
@@ -3,16 +3,36 @@ package tracker;
 /*
  * Test
  *
- *@author Jevgenijs
+ *@author Jevgenijs A.
  *@version $Id$
  *@since 0.1
  */
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void back() {
+        System.setOut(this.stdout);
+    }
+
+
     @Test
     public void whenAddNewItem() {
         Tracker tracker = new Tracker();
@@ -40,5 +60,74 @@ public class StartUITest {
         Input input = new StubInput(answers);
         new StartUI(new Actions(new Decoration(), tracker, input)).init();
         assertThat(tracker.findAll().length, is(0));
+    }
+
+    @Test
+    public void whenShowAllItems() {
+        Tracker tracker = new Tracker();
+        String[] answers = {"0", "Vova", "is my friend", "1", "6"};
+        Input input = new StubInput(answers);
+        new StartUI(new Actions(new Decoration(), tracker, input)).init();
+        StringBuilder uiText = new StringBuilder();
+        uiText.append("**** MENU: SHOW ALL ITEMS ****");
+        uiText.append(System.lineSeparator());
+        uiText.append("++ item N1");
+        uiText.append(System.lineSeparator());
+        uiText.append("ID: ");
+        uiText.append(tracker.findAll()[0].getId());
+        uiText.append(System.lineSeparator());
+        uiText.append("Name: Vova");
+        uiText.append(System.lineSeparator());
+        uiText.append("Description: is my friend");
+        uiText.append(System.lineSeparator());
+        uiText.append("---------");
+        uiText.append(System.lineSeparator());
+        String cutAnswer = new String(out.toByteArray());
+        assertThat(cutAnswer.substring(673, 786), is(uiText.toString()));
+    }
+
+    @Test
+    public void whenFIBI() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Vova", "is my friend", 2L);
+        tracker.add(item);
+        String id = tracker.findAll()[0].getId();
+        String[] answers = {"4", id, "6"};
+        Input input = new StubInput(answers);
+        new StartUI(new Actions(new Decoration(), tracker, input)).init();
+        StringBuilder uiText = new StringBuilder();
+        uiText.append("ID: ");
+        uiText.append(tracker.findAll()[0].getId());
+        uiText.append(System.lineSeparator());
+        uiText.append("Name: Vova");
+        uiText.append(System.lineSeparator());
+        uiText.append("Description: is my friend");
+        uiText.append(System.lineSeparator());
+        uiText.append("---------");
+        uiText.append(System.lineSeparator());
+        String cutAnswer = new String(out.toByteArray());
+        assertThat(cutAnswer.substring(395, 464), is(uiText.toString()));
+    }
+
+    @Test
+    public void whenFIBN() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Vova", "is my friend", 2L);
+        tracker.add(item);
+        String[] answers = {"5", "Vova", "6"};
+        Input input = new StubInput(answers);
+        new StartUI(new Actions(new Decoration(), tracker, input)).init();
+        StringBuilder uiText = new StringBuilder();
+        uiText.append("ID: ");
+        uiText.append(tracker.findAll()[0].getId());
+        uiText.append(System.lineSeparator());
+        uiText.append("Name: Vova");
+        uiText.append(System.lineSeparator());
+        uiText.append("Description: is my friend");
+        uiText.append(System.lineSeparator());
+        uiText.append("---------");
+        uiText.append(System.lineSeparator());
+        String cutAnswer = new String(out.toByteArray());
+        assertThat(cutAnswer.substring(329, 398), is(uiText.toString()));
     }
 }
